93e042ca052d2b2eed3a84957c33f9ebc2d62e4d
Edward Wilde <ewilde@gmail.com>
2017-05-05 22:36:30 +0100

    remove client code

---
+++ builtin/providers/runscope/resource_runscope_test_item_test.go
@@ -0,0 +1,87 @@
+package runscope
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"os"
+	"testing"
+)
+
+var _ = fmt.Sprintf("dummy") // dummy
+var _ = os.DevNull           // dummy
+
+func TestAccBucket_basic(t *testing.T) {
+	var bucketResponse response
+	teamId := os.Getenv("RUNSCOPE_TEAM_ID")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckBucketDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: fmt.Sprintf(testRunscopeBucketConfigA, teamId),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckBucketExists("runscope_bucket.test", &bucketResponse),
+					resource.TestCheckResourceAttr(
+						"runscope_bucket.test", "name", "terraform-provider-test"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckBucketDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "runscope_bucket" {
+			continue
+		}
+
+		_, err := client.ReadBucket(rs.Primary.ID)
+
+		if err == nil {
+			return fmt.Errorf("Record %s still exists", rs.Primary.ID)
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckBucketExists(n string, bucketResponse *response) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No Record ID is set")
+		}
+
+		client := testAccProvider.Meta().(*Client)
+
+		foundRecord, err := client.ReadBucket(rs.Primary.ID)
+
+		if err != nil {
+			return err
+		}
+
+		if foundRecord.Data["key"] != rs.Primary.ID {
+			return fmt.Errorf("Record not found")
+		}
+
+		*bucketResponse = foundRecord
+
+		return nil
+	}
+}
+
+const testRunscopeBucketConfigA = `
+resource "runscope_bucket" "test" {
+  name = "terraform-provider-test"
+  team_uuid = "%s"
+}`
