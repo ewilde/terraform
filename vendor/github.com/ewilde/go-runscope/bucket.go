1e73e727ee831154d94c3a43c520334d9a9c0c19
Edward Wilde <ewilde@gmail.com>
2017-05-08 07:45:17 +0100

    update documentation for runscope test resource

---
+++ vendor/github.com/ewilde/go-runscope/bucket.go
@@ -0,0 +1,76 @@
+package runscope
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/url"
+)
+
+type Bucket struct {
+	Name           string `json:"name,omitempty"`
+	Key            string `json:"key,omitempty"`
+	Default        bool   `json:"default,omitempty"`
+	AuthToken      string `json:"auth_token,omitempty"`
+	TestsUrl       string `json:"tests_url,omitempty" mapstructure:"tests_url"`
+	CollectionsUrl string `json:"collections_url,omitempty"`
+	MessagesUrl    string `json:"messages_url,omitempty"`
+	TriggerUrl     string `json:"trigger_url,omitempty"`
+	VerifySsl      bool   `json:"verify_ssl,omitempty"`
+	Team           Team   `json:"team,omitempty"`
+}
+
+func (client *Client) CreateBucket(bucket Bucket) (*Bucket, error) {
+	log.Printf("[DEBUG] creating bucket %s", bucket.Name)
+	data := url.Values{}
+	data.Add("name", bucket.Name)
+	data.Add("team_uuid", bucket.Team.Id)
+
+	log.Printf("[DEBUG] 	request: POST %s %#v", "/buckets", data)
+
+	req, err := client.newFormUrlEncodedRequest("POST", "/buckets", data)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.Http.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, _ := ioutil.ReadAll(resp.Body)
+	bodyString := string(bodyBytes)
+	log.Printf("[DEBUG] 	response: %d %s", resp.StatusCode, bodyString)
+
+	if resp.StatusCode >= 300 {
+		errorResp := new(errorResponse)
+		if err = json.Unmarshal(bodyBytes, &errorResp); err != nil {
+			return nil, fmt.Errorf("Error creating bucket: %s", bucket.Name)
+		} else {
+			return nil, fmt.Errorf("Error creating bucket: %s, status: %d reason: %q", bucket.Name,
+				errorResp.Status, errorResp.ErrorMessage)
+		}
+	} else {
+		response := new(response)
+		json.Unmarshal(bodyBytes, &response)
+		return getBucketFromResponse(response.Data)
+	}
+}
+
+func (client *Client) ReadBucket(key string) (*Bucket, error) {
+	resource, error := client.readResource("bucket", key, fmt.Sprintf("/buckets/%s", key))
+	bucket, error := getBucketFromResponse(resource.Data)
+	return bucket, error
+}
+
+func (client *Client) DeleteBucket(key string) error {
+	return client.deleteResource("bucket", key, fmt.Sprintf("/buckets/%s", key))
+}
+
+func getBucketFromResponse(response interface{}) (*Bucket, error) {
+	bucket := new(Bucket)
+	err := Decode(bucket, response)
+	return bucket, err
+}
---
+++ vendor/github.com/ewilde/go-runscope/test.go
@@ -0,0 +1,74 @@
+package runscope
+
+import (
+	"fmt"
+	"time"
+
+)
+
+type Test struct {
+	Id                 string    `json:"id,omitempty"`
+	Bucket             *Bucket   `json:"-"`
+	Name               string    `json:"name"`
+	Description        string    `json:"description"`
+	CreatedAt          time.Time `json:"created_at"`
+	CreatedBy          Contact   `json:"created_by"`
+	DefaultEnvironment string    `json:"default_environment_id"`
+	ExportedAt         time.Time `json:"exported_at"`
+}
+
+type Contact struct {
+	Email         string     `json:"email"`
+        Id            string     `json:"id"`
+        Name          string     `json:"name"`
+}
+
+func NewTest() *Test {
+	return &Test { Bucket: &Bucket{}}
+}
+
+func (client *Client) CreateTest(test *Test) (*Test, error) {
+	newResource, error := client.createResource(test, "test", test.Name,
+		fmt.Sprintf("/buckets/%s/tests", test.Bucket.Key))
+	if error != nil {
+		return nil, error
+	}
+
+	newTest, error := getTestFromResponse(newResource.Data)
+	if error != nil {
+		return nil, error
+	}
+
+	newTest.Bucket = test.Bucket
+	return newTest, nil
+}
+
+func (client *Client) ReadTest(test *Test) (*Test, error) {
+	resource, error := client.readResource("test", test.Id, fmt.Sprintf("/buckets/%s/tests/%s", test.Bucket.Key, test.Id))
+	if error != nil {
+		return nil, error
+	}
+
+	readTest, error := getTestFromResponse(resource.Data)
+	if error != nil {
+		return nil, error
+	}
+
+	readTest.Bucket = test.Bucket
+	return readTest, nil
+}
+
+func (client *Client) UpdateTest(test *Test) (response, error) {
+	resource, error := client.updateResource(test, "test", test.Id, fmt.Sprintf("/buckets/%s/tests/%s", test.Bucket.Key, test.Id))
+	return resource.(response), error
+}
+
+func (client *Client) DeleteTest(test *Test) error {
+	return client.deleteResource("test", test.Id, fmt.Sprintf("/buckets/%s/tests/%s", test.Bucket.Key, test.Id))
+}
+
+func getTestFromResponse(response interface{}) (*Test, error) {
+	test := new(Test)
+	err := Decode(test, response)
+	return test, err
+}
